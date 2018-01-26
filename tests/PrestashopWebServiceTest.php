96be025d17fe0aed356b4564a286b43ca2ef27cd
pimlie <pimlie@hotmail.com>
2018-01-26 22:52:30 +0100

    Add tests

--- tests/PrestashopWebServiceTest.php
+++ tests/PrestashopWebServiceTest.php
@@ -1,18 +1,109 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+namespace Protechstudio\PrestashopWebService\Tests;
+
 use Protechstudio\PrestashopWebService\PrestashopWebService;
+use Protechstudio\PrestashopWebService\PrestashopWebServiceException;
+use Protechstudio\PrestashopWebService\PrestashopWebServiceLibrary;
+use Prestashop;
 
 class PrestashopWebServiceTest extends TestCase
 {
-
-    /**
-     * @test
-     */
+    /** @test */
     public function it_is_correctly_installed()
     {
         $this->assertInstanceOf(PrestashopWebService::class, Prestashop::getFacadeRoot());
     }
+    
+    /** @test */
+    public function it_can_perform_a_get_request()
+    {
+        $requestResponseStub = require('requests/category-schema.php');
+        $ps = $this->getMockedLibrary('executeCurl', $requestResponseStub);
+
+        $xml = $ps->get(['resource' => 'categories']);
+        
+        $this->assertEquals('prestashop', $xml->getName());
+        $this->assertEquals('category', $xml->children()[0]->getName());
+    }
+
+    /** @test */
+    public function it_throws_exception_on_404()
+    {
+        $ps = $this->getMockedLibrary('executeRequest', [
+                'status_code' => 404,
+                'response' => '',
+                'header' => ''
+            ]);
+
+        $this->expectException(PrestashopWebServiceException::class);
+        $xml = $ps->get(['resource' => 'categories']);
+    }
+
+    /** @test */
+    public function it_throws_exception_on_empty_response()
+    {
+        $ps = $this->getMockedLibrary('executeRequest', [
+                'status_code' => 200,
+                'response' => '',
+                'header' => ''
+            ]);
+
+        $this->expectExceptionMessage('HTTP response is empty', PrestashopWebServiceException::class);
+        $xml = $ps->get(['resource' => 'categories']);
+    }
+
+    /** @test */
+    public function it_throws_exception_on_malformed_xml()
+    {
+        $ps = $this->getMockedLibrary('executeRequest', [
+                'status_code' => 200,
+                'response' => '<malformed>',
+                'header' => ''
+            ]);
+
+        $this->expectExceptionMessage('HTTP XML response is not parsable', PrestashopWebServiceException::class);
+        $xml = $ps->get(['resource' => 'categories']);
+    }
+
+    /** @test */
+    public function it_throws_exception_on_unsupported_version()
+    {
+        $this->expectExceptionMessage('This library is not compatible with this version of PrestaShop', PrestashopWebServiceException::class);
+        Prestashop::isPrestashopVersionSupported('0.0.0.0');
+        Prestashop::isPrestashopVersionSupported('99.99.99.9999');
+    }
+
+    /** @test */
+    public function it_throws_exception_on_unsupported_version_from_request()
+    {
+        $requestResponseStub = require('requests/category-schema.php');
+        $requestResponseStub[0] = preg_replace('/^PSWS-Version:(.+?)$/im', 'PSWS-Version: 99.99.99.9999', $requestResponseStub[0]);
+        $ps = $this->getMockedLibrary('executeCurl', $requestResponseStub);
+
+        $this->expectExceptionMessage('This library is not compatible with this version of PrestaShop', PrestashopWebServiceException::class);
+        $xml = $ps->get(['resource' => 'categories']);
+    }
+
+    protected function getMockedLibrary($method = null, $returns = null)
+    {
+        $ps = $this->getMockBuilder(PrestashopWebServiceLibrary::class)
+            ->setConstructorArgs([
+                env('prestashop-webservice.url'),
+                env('prestashop-webservice.key'),
+                env('prestashop-webservice.debug'),
+            ]);
+
+        if (!$method) {
+            return $ps->getMock();
+        } else {
+            $ps = $ps->setMethods([$method])->getMock();
+
+            $ps->expects($this->once())
+                ->method($method)
+                ->willReturn($returns);
+            return $ps;
+        }
+    }
 }
+
---
+++ tests/config/prestashop-webservice.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'url' => 'http://example.com',
+    'token' => '1234',
+    'debug' => true
+];
